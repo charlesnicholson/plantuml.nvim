b944b3a62b1c96b99126688dc016e2e66cf14e5d
Charles Nicholson <charles.nicholson@gmail.com>
2025-08-22 08:59:20 -0400

    initial commit

---
+++ plugin/plantuml.lua
@@ -0,0 +1,19 @@
+local ok, plantuml = pcall(require, "plantuml")
+if not ok then
+  vim.notify("plantuml.nvim failed to load", vim.log.levels.ERROR)
+  return
+end
+
+plantuml.start()
+
+local augroup = vim.api.nvim_create_augroup("PlantUMLViewer", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "BufEnter", "TabEnter" }, {
+  group = augroup,
+  pattern = "*.puml",
+  callback = plantuml.update_diagram,
+  desc = "Update PlantUML diagram via WebSocket",
+})
+
+vim.api.nvim_create_user_command("PlantumlUpdate", function()
+  plantuml.update_diagram()
+end, { desc = "Manually trigger PlantUML update" })
